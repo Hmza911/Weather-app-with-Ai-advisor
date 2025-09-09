9fdbb5a25f901249adadf63efcd0e93001281b37
Hmza911 <calmehmza@gmail.com>
2025-09-09 17:50:01 +0500

    Updated layout and config files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.weatherapi.com",
+        protocol: "https",
+        port: "",
+        pathname: "/**",
+      },
+    ]
+  }
 };
 
 export default nextConfig;
